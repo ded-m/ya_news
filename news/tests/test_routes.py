49249757e37f969fe1ac9460b954cee5e141b99e
ded <89196994468@yandex.ru>
2023-06-20 21:01:23 +0300

    4.3

---
+++ news/tests/test_routes.py
@@ -0,0 +1,19 @@
+# news/tests/test_routes.py
+# Импортируем класс HTTPStatus.
+from http import HTTPStatus
+
+from django.test import TestCase
+from unittest import skip
+# Импортируем функцию reverse().
+from django.urls import reverse
+
+@skip()
+class TestRoutes(TestCase):
+
+    def test_home_page(self):
+        # Вместо прямого указания адреса 
+        # получаем его при помощи функции reverse().
+        url = reverse('news:home')
+        response = self.client.get(url)
+        # Проверяем, что код ответа равен статусу OK (он же 200).
+        self.assertEqual(response.status_code, HTTPStatus.OK)
